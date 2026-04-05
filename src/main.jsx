e62ea84500919ca5c0f20cffbb7c0938aa3f3474
ssidharth448 <ssidharth448@gmail.com>
2026-04-05 17:18:58 +0530

    fix logout redirect

--- src/main.jsx
+++ src/main.jsx
@@ -3,55 +3,50 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import {
-ClerkProvider,
-SignIn,
-SignUp,
+  ClerkProvider,
+  SignIn,
+  SignUp,
 } from "@clerk/clerk-react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 const PUBLISHABLE_KEY =
-"pk_test_YWN0dWFsLWhhZ2Zpc2gtODEuY2xlcmsuYWNjb3VudHMuZGV2JA";
+  "pk_test_YWN0dWFsLWhhZ2Zpc2gtODEuY2xlcmsuYWNjb3VudHMuZGV2JA";
+
 createRoot(document.getElementById("root")).render(
-<StrictMode>
-<ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-<BrowserRouter>
-<Routes>
-{/* Main App */}
-<Route path="/*" element={<App />} />
-{/* Sign In */}
-<Route
-path="/sign-in/*"
-element={
-<div className="auth-container">
-<div className="auth-box">
-<h1>evol</h1>
-<p>Collect words you love</p>
-<SignIn
-  routing="path"
-  path="/sign-in"
-  signUpUrl="/sign-up"
-/>
-</div>
-</div>
-}
-/>
-{/* Sign Up */}
-<Route
-path="/sign-up/*"
-element={
-<div className="auth-container">
-<div className="auth-box">
-<h1>evol</h1>
-<p>Collect words you love</p>
-<SignUp
-routing="path"
-path="/sign-up"
-/>
-</div>
-</div>
-}
-/>
-</Routes>
-</BrowserRouter>
-</ClerkProvider>
-</StrictMode>
+  <StrictMode>
+    <ClerkProvider 
+      publishableKey={PUBLISHABLE_KEY}
+      afterSignOutUrl="/sign-in"
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+          <Route
+            path="/sign-in/*"
+            element={
+              <div className="auth-container">
+                <div className="auth-box">
+                  <h1>evol</h1>
+                  <p>Collect words you love</p>
+                  <SignIn routing="path" path="/sign-in" signUpUrl="/sign-up" />
+                </div>
+              </div>
+            }
+          />
+          <Route
+            path="/sign-up/*"
+            element={
+              <div className="auth-container">
+                <div className="auth-box">
+                  <h1>evol</h1>
+                  <p>Collect words you love</p>
+                  <SignUp routing="path" path="/sign-up" />
+                </div>
+              </div>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </ClerkProvider>
+  </StrictMode>
 );
